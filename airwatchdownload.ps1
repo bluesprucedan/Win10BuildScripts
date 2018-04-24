1b4b4ac2677af9baef7aac4b2dc5c2bb519fc414
bluesprucedan <37852800+bluesprucedan@users.noreply.github.com>
2018-04-24 08:53:31 -0600

    Update airwatchdownload.ps1

--- airwatchdownload.ps1
+++ airwatchdownload.ps1
@@ -1,4 +1,4 @@
-powershell -command “& { iwr https://awagent.com/Home/DownloadWinPcAgentApplication -OutFile C:\Windows\Temp\AirWatchAgent.msi }”
+powershell -command "& { iwr https://awagent.com/Home/DownloadWinPcAgentApplication -OutFile C:\Windows\Temp\AirWatchAgent.msi }"
 
 Start-Process C:\Windows\Temp\AirWatchAgent.msi /passive
 #Mess around with file location
